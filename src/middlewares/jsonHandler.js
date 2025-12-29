5a9cec5ece7c1a63ccfe718a9cbe08b6e6a07797
Gabriel <brcr873@gmail.com>
2025-12-29 00:01:48 -0300

    first-commit

---
+++ src/middlewares/jsonHandler.js
@@ -0,0 +1,14 @@
+export async function jsonHandler(resquest, response) {
+    const buffers = []
+    for await (const chunk of resquest) {
+        buffers.push(chunk)
+    }
+
+    try {
+        resquest.body = JSON.parse(Buffer.concat(buffers).toString())
+    } catch {
+        resquest.body = null
+    }
+
+    response.setHeader("Content-Type", "application/json")
+}
